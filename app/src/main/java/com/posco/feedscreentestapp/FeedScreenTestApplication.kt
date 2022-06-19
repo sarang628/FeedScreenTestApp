6f7b7ea3d4da234426e40ebda3ecc2ab05125cf4
Sarang Yang <sarang628@naver.com>
2022-06-19 13:02:19 +0900

    의존성 모듈 추가 및 프로젝트 초기화 Add dependency modules and initialize the project

---
+++ app/src/main/java/com/posco/feedscreentestapp/FeedScreenTestApplication.kt
@@ -0,0 +1,8 @@
+package com.posco.feedscreentestapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FeedScreenTestApplication : Application() {
+}
