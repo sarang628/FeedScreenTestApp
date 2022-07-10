3072d9215983e80e94327a596e4e0f0130d17f6d
Sarang Yang <sarang628@naver.com>
2022-07-10 14:02:41 +0900

    update

---
+++ app/src/main/java/com/posco/feedscreentestapp/FeedLayoutTestActivity.kt
@@ -0,0 +1,60 @@
+package com.posco.feedscreentestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.screen_feed.FeedsFragmentLayoutUseCase
+import com.example.screen_feed.databinding.FragmentFeedsBinding
+import com.google.android.material.snackbar.Snackbar
+
+class FeedLayoutTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<FragmentFeedsBinding>(
+            this,
+            com.example.screen_feed.R.layout.fragment_feeds
+        )
+
+        val useCase = FeedsFragmentLayoutUseCase(
+            onRefreshListener = {
+                Snackbar.make(binding.root, "갱신 클릭!", Toast.LENGTH_SHORT).show()
+            },
+            onMenuItemClickListener = {
+                Snackbar.make(binding.root, "리뷰작성 클릭!", Toast.LENGTH_SHORT).show()
+                false
+            },
+            adapter = TestAdapter(),
+            reLoad = {
+                Snackbar.make(binding.root, "갱신!", Toast.LENGTH_SHORT).show()
+                binding.slTimeline.isRefreshing = false
+            }
+        )
+
+        binding.reload = useCase.reLoad
+        binding.onMenuItemClickListener = useCase.onMenuItemClickListener
+        binding.onRefreshListener = useCase.onRefreshListener
+        binding.adapter = TestAdapter()
+
+    }
+
+    class TestAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return object : RecyclerView.ViewHolder(TextView(parent.context)) {
+
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+    }
+}
