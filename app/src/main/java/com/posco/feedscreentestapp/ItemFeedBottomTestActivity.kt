e75b7589b95ffeb19f5022cff565a39b8f30f333
sryang <sarang628@naver.com>
2022-07-12 09:16:29 +0900

    update

---
+++ app/src/main/java/com/posco/feedscreentestapp/ItemFeedBottomTestActivity.kt
@@ -0,0 +1,11 @@
+package com.posco.feedscreentestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemFeedBottomTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.screen_feed.R.layout.item_feed_bottom)
+    }
+}
